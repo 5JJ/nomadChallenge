e338733f91a3b97a3eecc8657fc91c0fd7496c7a
JeonghuiOh <jeonghui.oh@reebonz.co.kr>
2021-04-15 02:03:38 +0900

    fetch data when component mount, not click the question mark icon(delete)

--- src/Components/Tabs/Production.js
+++ src/Components/Tabs/Production.js
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import PropTypes from "prop-types";
 import styled, { keyframes } from "styled-components";
 import NoResult from "../NoResult";
 import {companyApi} from "api";
+import Message from "Components/Message";
 
 const Container = styled.div``;
 const CompanyList = styled.ul``;
@@ -44,37 +45,19 @@ const CountryIcons = {
     'CA': '🇨🇦',
     'KR': '🇰🇷',
 }
-const QuestionIcon = styled.i`
-    border-radius: 50%;
-    background-color: #999;
-    display: inline-block;
-    width: 16px;
-    height: 16px;
-    line-height: 16px;
-    text-align: center;
-    color: white;
-    font-size: 14px;
-    cursor: pointer;
-
-    &:hover{
-        background-color: #5e5c5c;
-    }
-`;
 
 const InfoContainer = styled.div`
     width: 100%;
 `;
-const DetailShowUp = keyframes`
-    0% {top: 100%}
-    4% {top: 0%}
-    33.33% {top: 0%}
-    37.33% {top: -100%}
-    100% {top: -100%}
-`
+
 const DetailInfo = styled.dl`
     clear: both;
     line-height: 1.6;
     color: #999;
+    
+    &:first-child{
+        margin-top: 12px;
+    }
 `;
 const DetailTitle = styled.dt`
     float: left;
@@ -82,29 +65,43 @@ const DetailTitle = styled.dt`
     font-weight: bold;
 `;
 
-const CompanyDetail = () => {
+const LoadingFailed = styled(Message)`
+    &&& {
+        width: 100%;
+        margin-top: 10px;
+        justify-content: left;
+    }
+`;
+
+const CompanyDetail = (companyIds) => {
     const [detailList, setDetailList] = useState({})
-    const [loading, setLoading] = useState(false);
-    const getCompanyDetail = async (id) => {
-        try{
-            setLoading(true);
-            if(!detailList[id.toString()] && !loading){
+    const companyIdsCopied = [...companyIds];
+    const getCompanyDetail = async () => {
+        
+        for (const id of companyIdsCopied) {
+            const newState = {};
+            try{
                 const {data} = await companyApi.getDetail(id);
-                const newState = {};
                 newState[id.toString()] = data;
                 setDetailList((prevState) => ({...prevState, ...newState}));
-            }
-        }catch(error){
+            }catch(error){
+                newState[id.toString()] = "Loading Failed";
+                setDetailList((prevState) => ({...prevState, ...newState}));
+            }finally{
 
-        }finally{
-            setLoading(false);
+            }
         }
     }
-    return {detailList, getCompanyDetail, loading}
+    
+    useEffect(() => {
+        getCompanyDetail();
+        return () => {console.log('bye')};
+    }, [])
+    return detailList
 }
 
 const ProductionTab = ({companies}) => {
-    const {detailList, getCompanyDetail, loading} = CompanyDetail();
+    const detailList = CompanyDetail(companies.map(company => company.id));
     return (companies && companies.length > 0 ?
     <CompanyList>
         {companies && companies.map((company, index) => 
@@ -114,13 +111,15 @@ const ProductionTab = ({companies}) => {
             <InfoContainer>
                 <Name>{company.name}</Name>
                 {company.origin_country && <Country title={company.origin_country}>{`(${CountryIcons[company.origin_country] ? CountryIcons[company.origin_country] : company.origin_country})`}</Country>}
-                <QuestionIcon onClick={(e) => getCompanyDetail(company.id)} disabled={loading} aria-label="more company Info">?</QuestionIcon>
                 {detailList[company.id.toString()] && (
+                    typeof detailList[company.id.toString()] === "object" ?
                     <div>
                         <DetailInfo><DetailTitle>Hompage: </DetailTitle><dd>{detailList[company.id.toString()].homepage}</dd></DetailInfo>
                         <DetailInfo><DetailTitle>HQ: </DetailTitle><dd>{detailList[company.id.toString()].headquarters}</dd></DetailInfo>
                         <DetailInfo><DetailTitle>Description: </DetailTitle><dd>{detailList[company.id.toString()].description}</dd></DetailInfo>
                     </div>
+                    :
+                    <LoadingFailed text="Loading Failed 🚫" color="#ddd"/>
                 )}
             </InfoContainer>
         </Company>))} 
