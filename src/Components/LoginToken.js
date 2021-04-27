24bb9664fb8754cadc5e8dec8a151ee1f26d39d9
5JJ <nedin12@naver.com>
2021-04-27 17:07:49 +0900

    [feat] export getToken and getSessionId func - need to export it to check whether request token has created

--- src/Components/LoginToken.js
+++ src/Components/LoginToken.js
@@ -1,17 +1,30 @@
 import { useState } from "react";
 
+export const getToken = () => {
+  return window.localStorage.getItem("token");
+};
+export const getSessionId = () => {
+  return window.localStorage.getItem("sessionId");
+};
 const LoginToken = () => {
-  const getToken = () => {
-    return window.localStorage.getItem("token");
-  };
-  const [token, setToken] = useState(getToken());
+  const [token, setToken] = useState(getToken() || "");
+  const [sessionId, setSessionId] = useState(getSessionId() || "");
 
   const saveToken = (value) => {
     window.localStorage.setItem("token", value);
     setToken(value);
   };
+  const saveSessionId = (value) => {
+    window.localStorage.setItem("sessionId", value);
+    setSessionId(value);
+  };
 
-  return { setToken: saveToken, token };
+  return {
+    setToken: saveToken,
+    token,
+    setSessionId: saveSessionId,
+    isLogin: !!sessionId,
+  };
 };
 
 export default LoginToken;
