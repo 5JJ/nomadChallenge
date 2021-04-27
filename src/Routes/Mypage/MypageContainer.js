78d7e980cb3a07d1b04ff0afa7cca37a76f7882f
5JJ <nedin12@naver.com>
2021-04-27 18:14:55 +0900

    [feat] if not logged in, redirect to login page

--- src/Routes/Mypage/MypageContainer.js
+++ src/Routes/Mypage/MypageContainer.js
@@ -2,9 +2,8 @@ import React from "react";
 import LoginToken from "Components/LoginToken";
 import MypagePresenter from "./MypagePresenter";
 const MypageContainer = () => {
-  const { token, setToken } = LoginToken();
-  console.log(token);
-  return <MypagePresenter token={token} />;
+  const { isLogin } = LoginToken();
+  return <MypagePresenter isLogin={isLogin} />;
 };
 
 export default MypageContainer;
--- src/Routes/Mypage/MypagePresenter.js
+++ src/Routes/Mypage/MypagePresenter.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
-const MypagePresenter = ({ token }) => {
-  return token ? (
+const MypagePresenter = ({ isLogin }) => {
+  return isLogin ? (
     "Mypage"
   ) : (
     <Redirect to={{ pathname: "/login", state: { from: "/mypage" } }} />
